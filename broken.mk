09ff27a4cee1a82e52bfc7596431becf2c300f69
MasterChief87 <mc87xda@gmail.com>
2015-12-13 22:42:52 -0500

    Update and rename cm.mk to broken.mk

--- broken.mk
+++ broken.mk
@@ -8,6 +8,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_h631
+PRODUCT_NAME := broken_h631
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g4stylusn" PRODUCT_NAME="g4stylusn_tmo_us" BUILD_FINGERPRINT="lge/g4stylusn_tmo_us/g4stylusn:5.1.1/LMY47V/1521710042a4b:user/release-keys" PRIVATE_BUILD_DESC="g4stylusn_tmo_us-user 5.1.1 LMY47V 1521710042a4b release-keys"
